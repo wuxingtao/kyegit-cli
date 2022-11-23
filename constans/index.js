0161ef70cf8186164ab2583ff35c6ee9e3631465
xingtao <xingtaowap@gmail.com>
2022-11-23 09:55:32 +0800

    feat: update git lists

--- constans/index.js
+++ constans/index.js
@@ -6,33 +6,39 @@
 export const gitLists = [
   {
     name: 'netc-pc-web',
-    dir: 'D:\\gitlab\\netc-pc-web',
+    dir: 'D:\\gitlab\\kyegit-lists\\netc-pc-web',
     branchPrefix: 'gray',
     git: 'git@gitlab.ky-tech.com.cn:KYE-NETC-FRONTEND/netc-pc-web.git'
   },
   {
     name: 'netc-h5-modules',
-    dir: 'D:\\gitlab\\netc-h5-modules',
+    dir: 'D:\\gitlab\\kyegit-lists\\netc-h5-modules',
     branchPrefix: 'test',
     git: 'git@gitlab.ky-tech.com.cn:KYE-NETC-FRONTEND/netc-h5-modules.git'
   },
   {
     name: 'website',
-    dir: 'D:\\gitlab\\website',
+    dir: 'D:\\gitlab\\kyegit-lists\\website',
     branchPrefix: 'test',
     git: 'git@gitlab.ky-tech.com.cn:KYE-NETC-FRONTEND/website.git'
   },
   {
     name: 'website-static-page',
-    dir: 'D:\\gitlab\\website-static-page',
+    dir: 'D:\\gitlab\\kyegit-lists\\website-static-page',
     branchPrefix: 'test',
     git: 'git@gitlab.ky-tech.com.cn:KYE-NETC-FRONTEND/website-static-page.git'
   },
   {
     name: 'ERP-EAM',
-    dir: 'D:\\gitlab\\KYE-ERP\\base\\src\\eam',
+    dir: 'D:\\gitlab\\kyegit-lists\\eam',
     branchPrefix: 'prod',
     git: 'git@gitlab.ky-tech.com.cn:erp-frontend/eam.git'
-  }
+  },
+  {
+    name: 'netc-customer-code',
+    dir: 'D:\\gitlab\\kyegit-lists\\netc-customer-code',
+    branchPrefix: 'test',
+    git: 'git@gitlab.ky-tech.com.cn:KYE-NETC-FRONTEND/netc-customer-code.git'
+  },
 ]
 
